d61f38ed405009ac4564091715d8bed3b583b8cd
Raluca Nicola <rnicola@esri.com>
2018-07-07 19:04:52 +0200

    Check for WebGL supprt

--- urban-sketch/main.js
+++ urban-sketch/main.js
@@ -17,84 +17,94 @@ require([
     container: "viewDiv",
     map: webscene
   });
-  view.environment.lighting.directShadowsEnabled = true;
 
-  const sitePlanLayer = new SceneLayer({
-    // Data copyright: Houseal Lavigne Associates, LLC
-    url: "https://tiles.arcgis.com/tiles/74bZbbuf05Ctvbzv/arcgis/rest/services/SitePlan_Clean/SceneServer",
-    renderer: {
-      type: "simple",
-      symbol: {
-        type: "mesh-3d",
-        symbolLayers: [{
-          type: "fill",
-          material: {
-            color: "#ffffff",
-            colorMixMode: "tint"
-          },
-          edges: {
-            type: "sketch",
-            color: [0, 0, 0, 0.65],
-            extensionLength: 10,
-            size: 1
-          }
-        }]
-      }
-    },
-    popupEnabled: false
+  view.when(function() {
+
+    view.environment.lighting.directShadowsEnabled = true;
+    const sitePlanLayer = new SceneLayer({
+      // Data copyright: Houseal Lavigne Associates, LLC
+      url: "https://tiles.arcgis.com/tiles/74bZbbuf05Ctvbzv/arcgis/rest/services/SitePlan_Clean/SceneServer",
+      renderer: {
+        type: "simple",
+        symbol: {
+          type: "mesh-3d",
+          symbolLayers: [{
+            type: "fill",
+            material: {
+              color: "#ffffff",
+              colorMixMode: "tint"
+            },
+            edges: {
+              type: "sketch",
+              color: [0, 0, 0, 0.65],
+              extensionLength: 10,
+              size: 1
+            }
+          }]
+        }
+      },
+      popupEnabled: false
+    });
+
+    const buildingsLayer = new SceneLayer({
+      // Data copyright: Houseal Lavigne Associates, LLC
+      url: "https://tiles.arcgis.com/tiles/74bZbbuf05Ctvbzv/arcgis/rest/services/Buildings/SceneServer",
+      renderer: {
+        type: "simple",
+        symbol: {
+          type: "mesh-3d",
+          symbolLayers: [{
+            type: "fill",
+            material: {
+              color: "#ffffff",
+              colorMixMode: "replace"
+            },
+            edges: {
+              type: "sketch",
+              color: [0, 0, 0, 0.65],
+              extensionLength: 10,
+              size: 1
+            }
+          }]
+        }
+      },
+      popupEnabled: false
+    });
+
+    const proposedProjectLayer = new SceneLayer({
+      // Data copyright: Houseal Lavigne Associates, LLC
+      url: "https://tiles.arcgis.com/tiles/74bZbbuf05Ctvbzv/arcgis/rest/services/Option2_r/SceneServer",
+      renderer: {
+        type: "simple",
+        symbol: {
+          type: "mesh-3d",
+          symbolLayers: [{
+            type: "fill",
+            material: {
+              color: "#ffffff",
+              colorMixMode: "replace"
+            },
+            edges: {
+              type: "sketch",
+              color: [0, 0, 0, 0.65],
+              extensionLength: 10,
+              size: 1
+            }
+          }]
+        }
+      },
+      popupEnabled: false
+    });
+
+    webscene.addMany([sitePlanLayer, proposedProjectLayer, buildingsLayer]);
+
+    if (isMobile()) {
+      view.ui.empty("top-left");
+    }
+  })
+  .catch(function() {
+    document.getElementById("viewDiv").innerHTML = "<h2>Sorry, it seems your browser doesn't support WebGL :(</h2>";
   });
-
-  const buildingsLayer = new SceneLayer({
-    // Data copyright: Houseal Lavigne Associates, LLC
-    url: "https://tiles.arcgis.com/tiles/74bZbbuf05Ctvbzv/arcgis/rest/services/Buildings/SceneServer",
-    renderer: {
-      type: "simple",
-      symbol: {
-        type: "mesh-3d",
-        symbolLayers: [{
-          type: "fill",
-          material: {
-            color: "#ffffff",
-            colorMixMode: "replace"
-          },
-          edges: {
-            type: "sketch",
-            color: [0, 0, 0, 0.65],
-            extensionLength: 10,
-            size: 1
-          }
-        }]
-      }
-    },
-    popupEnabled: false
-  });
-
-  const proposedProjectLayer = new SceneLayer({
-    // Data copyright: Houseal Lavigne Associates, LLC
-    url: "https://tiles.arcgis.com/tiles/74bZbbuf05Ctvbzv/arcgis/rest/services/Option2_r/SceneServer",
-    renderer: {
-      type: "simple",
-      symbol: {
-        type: "mesh-3d",
-        symbolLayers: [{
-          type: "fill",
-          material: {
-            color: "#ffffff",
-            colorMixMode: "replace"
-          },
-          edges: {
-            type: "sketch",
-            color: [0, 0, 0, 0.65],
-            extensionLength: 10,
-            size: 1
-          }
-        }]
-      }
-    },
-    popupEnabled: false
-  });
-
-  webscene.addMany([sitePlanLayer, proposedProjectLayer, buildingsLayer]);
 
   function isMobile() {
     if (has("ui-mode:phone") || has("ui-mode:tablet")) {
@@ -102,8 +112,4 @@ require([
     }
     return /Android|webOS|iPhone|iPad|iPod|BlackBerry|Opera Mini|IEMobile/i.test(navigator.userAgent);
   }
-  if (isMobile()) {
-    view.ui.empty("top-left");
-  }
-
 });
