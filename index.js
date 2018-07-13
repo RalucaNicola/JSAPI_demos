cc6968d58dc767a16d0d0646db34bb89d7207324
Raluca Nicola <rauc8872@esri.com>
2018-07-13 15:22:04 +0200

    Update package file

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ var bs = require("browser-sync").create('demos-server');
 
 // .init starts the server
 bs.init({
-    server: "./scene-swiper"
+    server: "./world-population-2020/"
 });
 
 // Now call methods on bs
-bs.watch('./scene-swiper/app/*').on('change', bs.reload);
+bs.watch('./world-population-2020/app/*').on('change', bs.reload);
